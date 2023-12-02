507160437eb73ff7e418ba6c84ccc3807bde87aa
VictorGstn <125529445+VictorGstn@users.noreply.github.com>
2023-12-02 12:22:15 +0700

    Add files via upload

---
+++ application/views/templates_admin/footer.php
@@ -0,0 +1,24 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="...">
+
+<!-- Bootstrap JS (Popper.js and Bootstrap JS) -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="..."></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="..."></script>
+
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?php echo base_url() ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?php echo base_url() ?>assets/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="<?php echo base_url() ?>assets/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="<?php echo base_url() ?>assets/js/demo/chart-area-demo.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
